aaf7b57d0e283063a9e68eb5ab666de6bf5c1c11
ameliadai <hd2584@nyu.edu>
2025-06-17 16:43:36 -0400

    update eval script

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -3,14 +3,8 @@ import pandas as pd
 import os
 import re
 from tqdm import tqdm
-import matplotlib.dates as mdates
-from datetime import datetime
-import random
-import numpy as np
-import torch
 from openai import OpenAI
 import anthropic
-from transformers import AutoModelForCausalLM, AutoTokenizer
 from vllm import LLM
 from vllm.sampling_params import SamplingParams
 
@@ -22,14 +16,12 @@ MODEL_MAPPING = {
     'gpt-4o-mini': 'gpt-4o-mini-2024-07-18',
     'gpt-4o': 'gpt-4o-2024-11-20',
     'claude-3-5-sonnet': 'claude-3-5-sonnet-20240620',
-    'llama3': 'Meta-Llama-3-8B-Instruct-hf',
+    'llama3': 'meta-llama/Meta-Llama-3-8B-Instruct',
     'mistral-7b': 'mistralai/Mistral-7B-Instruct-v0.3',
     'mixtral-8x7b': 'mistralai/Mixtral-8x7B-Instruct-v0.1',
     'gemma2-2b': 'google/gemma-2-2b-it',
     'qwen2-7b': 'Qwen/Qwen2-7B-Instruct',
     }
-
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def load_model(model_name, num_gpu=1):
@@ -38,26 +30,16 @@ def load_model(model_name, num_gpu=1):
         return OpenAI()
     elif 'claude' in model_name:
         return anthropic.Anthropic()
-    elif 'llama' in model_name:
-        LOCAL_CACHE_DIR = "" # your own local cache directory for llama weights
-        tokenizer = AutoTokenizer.from_pretrained(LOCAL_CACHE_DIR)
-        model = AutoModelForCausalLM.from_pretrained(
-            LOCAL_CACHE_DIR,
-            torch_dtype=torch.bfloat16,
-            device_map="auto",
-        )
-        return (tokenizer, model)
     else:    
         client = LLM(
             model=model_id,
-            download_dir="", # your own local cache directory for vllm weights
+            download_dir='', # your own local cache directory for vllm weights
             dtype="float16",
             tensor_parallel_size=num_gpu
         )
         return client
 
-
-def generate_response(model_name, client, prompt, max_tokens=1024):
+def generate_single_response(model_name, client, prompt, max_tokens=1024):
     model_id = MODEL_MAPPING[model_name]
     try:
         if 'gpt' in model_name:
@@ -75,27 +57,6 @@ def generate_response(model_name, client, prompt, max_tokens=1024):
                 messages=[{"role": "user", "content": [{"type": "text", "text": prompt}]}]
             )
             return message.content[0].text
-        elif 'llama' in model_name:
-            tokenizer, model = client
-            input_ids = tokenizer.apply_chat_template(
-                prompt,
-                add_generation_prompt=True,
-                return_tensors="pt"
-            ).to(model.device)
-
-            terminators = [
-                tokenizer.eos_token_id,
-                tokenizer.convert_tokens_to_ids("<|eot_id|>")
-            ]
-
-            outputs = model.generate(
-                input_ids,
-                max_new_tokens=max_tokens,
-                eos_token_id=terminators,
-                pad_token_id=tokenizer.eos_token_id
-            )
-            response = tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
-            return response
         else:
             sampling_params = SamplingParams(max_tokens=max_tokens)
             response = client.chat(messages=prompt, sampling_params=sampling_params, use_tqdm=False)
@@ -105,26 +66,37 @@ def generate_response(model_name, client, prompt, max_tokens=1024):
         print(f"- Failed prompt: {prompt}\n- Error: {e}")
         return None
 
-def get_prompt(eval_setting, model_name, question_type, question, choices = None, articles = None):
+def generate_multi_response(model_name, client, prompt_ls, max_tokens=1024):
+    response_ls = []
+    sampling_params = SamplingParams(max_tokens=max_tokens)
+    response = client.chat(messages=prompt_ls, sampling_params=sampling_params, use_tqdm=True)
+    for output in response:
+        generated_text = output.outputs[0].text
+        response_ls.append(generated_text)
+    return response_ls
+
+def get_prompt(eval_setting, model_name, question_type, row):
     if eval_setting == 'closed-book':
         if question_type == 'tf':
             prompt = f"""You should output your answer as either 'Yes' or 'No' WITHOUT anything else. 
 
-Question: {question}
+Question: {row['question']}
 Choices: 'Yes' or 'No'
 [Output:] Your answer: """
         elif question_type == 'mc':
             prompt = f"""You should output your answer as either '(a)', '(b)', '(c)' or '(d)' WITHOUT anything else.
 
-Question: {question}
+Question: {row['question']}
 Choices: 
-(a) {choices[0]}
-(b) {choices[1]}
-(c) {choices[2]}
-(d) {choices[3]}
+(a) {row['choice_a']}
+(b) {row['choice_b']}
+(c) {row['choice_c']}
+(d) {row['choice_d']}
 [Output:] Your answer: """
 
     elif eval_setting == 'open-book':
+        articles = row['retrieved_news']['text'][:5]
+        articles = [get_first_k_words(a) for a in articles]
         if question_type == 'tf':
             prompt = f"""You should output your answer as either 'Yes' or 'No' WITHOUT anything else. Below are the top 5 relevant news article fragments retrieved for the question, which may or may not assist you in making a forecast.
 Article 1: {articles[0]}
@@ -133,7 +105,7 @@ Article 3: {articles[2]}
 Article 4: {articles[3]}
 Article 5: {articles[4]}
 
-Question: {question}
+Question: {row['question']}
 Choices: 'Yes' or 'No'
 [Output:] Your answer: """
         elif question_type == 'mc':
@@ -144,32 +116,33 @@ Article 3: {articles[2]}
 Article 4: {articles[3]}
 Article 5: {articles[4]}
 
-Question: {question}
+Question: {row['question']}
 Choices: 
-(a) {choices[0]}
-(b) {choices[1]}
-(c) {choices[2]}
-(d) {choices[3]}
+(a) {row['choice_a']}
+(b) {row['choice_b']}
+(c) {row['choice_c']}
+(d) {row['choice_d']}
 [Output:] Your answer: """
 
     elif eval_setting == 'gold-article':
+        article = get_first_k_words(row['text'])
         if question_type == 'tf':
             prompt = f"""You should output your answer as either 'Yes' or 'No' WITHOUT anything else. Below is the updated news article relevant to the question, which may help you in providing an answer.
-Article: {articles}
+Article: {article}
 
-Question: {question}
+Question: {row['question']}
 Choices: 'Yes' or 'No'
 [Output:] Your answer: """
         elif question_type == 'mc':
             prompt = f"""You should output your answer as either '(a)', '(b)', '(c)' or '(d)' WITHOUT anything else. Below is the updated news article relevant to the question, which may help you in providing an answer.
-Article: {articles}
+Article: {article}
 
-Question: {question}
+Question: {row['question']}
 Choices: 
-(a) {choices[0]}
-(b) {choices[1]}
-(c) {choices[2]}
-(d) {choices[3]}
+(a) {row['choice_a']}
+(b) {row['choice_b']}
+(c) {row['choice_c']}
+(d) {row['choice_d']}
 [Output:] Your answer: """
 
     # format prompt        
@@ -187,29 +160,23 @@ def get_pred(eval_setting, model_name, client, data, question_type, save_path=No
     column_name = f'pred_{model_name}'
     if column_name not in data.columns:
         data[column_name] = None
+    
+    if 'gpt' in model_name or 'claude' in model_name:
+    # for proprietary model, we generate responses one by one
+        for idx, row in tqdm(data.iterrows(), total=len(data)):
+            if pd.isnull(row[column_name]): # do not regenerate answer
+                prompt = get_prompt(eval_setting, model_name, question_type, row)
+                response = generate_single_response(model_name, client, prompt)
+                data.at[idx, column_name] = response
+                print(response)
 
-    for idx, row in tqdm(data.iterrows(), total=len(data)):
-        if pd.isnull(row[column_name]): # do not regenerate answer
-            question = row['question']
-            if question_type == 'mc':
-                choices = [row['choice_a'], row['choice_b'], row['choice_c'], row['choice_d']]
-            else:
-                choices = None 
-            if eval_setting == 'closed-book':
-                articles = None
-            elif eval_setting == 'open-book':
-                articles = row['retrieved_news']['text'][:5]
-                articles = [get_first_k_words(a) for a in articles]
-            elif eval_setting == 'gold-article':
-                articles = get_first_k_words(row['text'])
-            prompt = get_prompt(eval_setting, model_name, question_type, question, choices, articles)
-            # print(prompt)
-            response = generate_response(model_name, client, prompt)
-            data.at[idx, column_name] = response
-            print(response)
-
-        if (idx+1) % 100 == 0 and save_path:
-            data.to_pickle(save_path)
+            if (idx+1) % 100 == 0 and save_path:
+                data.to_csv(save_path)
+    else:
+    # for open-source model, we generate responses in batch
+        prompt_ls = [get_prompt(eval_setting, model_name, question_type, row) for _, row in data.iterrows()]
+        response_ls = generate_multi_response(model_name, client, prompt_ls)
+        data[column_name] = response_ls
 
     return data
 
@@ -219,13 +186,12 @@ def get_first_k_words(text, k=512):
     result_text = ' '.join(first_words)
     return result_text
 
-
 def main(args):
     eval_setting = args.eval_setting
     model_name = args.model_name
     input_path = args.input_path
     output_path = args.output_path
-    
+
     question_type = 'tf' if 'tf' in input_path else 'mc'
     
     if eval_setting == 'open-book':
